bb63480a75b60486c06e5d2de9391d11c20eb06a
innTall <avg999.trade@gmail.com>
2023-10-01 15:51:45 +0200

    base header & footer templates

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,15 @@ import FooterBar from './components/FooterBar.vue';
 </script>
 
 <template>
-  <HeaderBar />
-  <RouterView />
-  <FooterBar />
+  <div class="w-full h-full bg-gray-800">
+    <HeaderBar class="w-full h-12 bg-gray-800 border-b-2 border-green-600 fixed
+		  left-0 top-0 flex items-center text-md z-10">
+    </HeaderBar>
+    <RouterView class="py-12" />
+    <FooterBar class="w-full h-10 bg-gray-800 text-gray-300 border-t-2 border-green-600 fixed
+      left-0 bottom-0 flex justify-center items-center text-md">
+    </FooterBar>
+  </div>
 </template>
 
 <style scoped></style>
